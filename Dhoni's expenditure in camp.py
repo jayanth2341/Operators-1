1793e5b7a87fafab04c95f65938875c90378b477
jayanth2341 <yjayanth769@gmail.com>
2024-10-22 11:21:31 +0530

    Update Dhoni's expenditure in camp.py

--- Dhoni's expenditure in camp.py
+++ Dhoni's expenditure in camp.py
@@ -5,19 +5,4 @@ Second line is an integer "Y" in km/litre that specifies the mileage of his frie
 Third line is a float "Z" that specifies the cost of petrol in rupees.
 Output Format:
 Output should display a float that gives the total amount that is needed by Dhoni to spend on his travel in rupees. The float value is displayed correct to 2 decimal places.
-Sample Input and Output 1:
-75
-55
-63
-2577.27
-Sample Input and Output 2:
-35
-78
-65.0
-875.00
-"""
-x=float(input())
-y=float(input())
-z=float(input())
-a=(x*z*30)/y
-print("%.2f"%a)
+Sample Input and 
